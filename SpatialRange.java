30d2d238cc4fd45fa9dd7ffc15be2087dda6bc9a
sagarburra <sagarburra007@outlook.com>
2015-11-07 22:38:26 -0700

    Update SpatialRange.java

--- SpatialRange.java
+++ SpatialRange.java
@@ -1,11 +1,8 @@
-package sample.sample1;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-
-import com.vividsolutions.jts.geom.*;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
@@ -13,21 +10,15 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.broadcast.Broadcast;
 
-    
+public class SpatialRange {
 
-public class SpatialRange
-{
-    private static final String String = null;
-
-    public static void main(String args[]) throws IOException
-    {
-    
-        SparkConf conf = new SparkConf().setMaster("local").setAppName("check");
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf().setMaster("local").setAppName("check");
         JavaSparkContext sc = new JavaSparkContext(conf);
         
         JavaRDD<String> input = sc.textFile(args[0]);
         String window = input.first();
-        String [] inputCoordinates=window.split(",");
+        String [] inputCoordinates = window.split(",");
         
         Double x1 =Double.parseDouble(inputCoordinates[0]);
         Double y1 =Double.parseDouble(inputCoordinates[1]);
@@ -36,25 +27,25 @@ public class SpatialRange
         
         Double[] rectangle={x1,y1,x2,y2};
         
-        Broadcast<Double[]> broadcastVar = sc.broadcast(window);
+        Broadcast<Double[]> broadcastVar = sc.broadcast(rectangle);
     	final Double[] broad = broadcastVar.value();
         
         input = sc.textFile(args[1]);
-        JavaRDD<Double> SpatialRangeOutput = input.mapPartitions(new FlatMapFunction<Iterator<String>, Double>() {
+        JavaRDD<Integer> SpatialRangeOutput = input.mapPartitions(new FlatMapFunction<Iterator<String>, Integer>() {
 
-            public Iterable<Double> call(Iterator<String> t) throws Exception {
-                ArrayList<String[]> inputCoordinates = new ArrayList<String[]>();
-                ArrayList<Double> outputpoints = new ArrayList<Double>();
+            public Iterable<Integer> call(Iterator<String> t) throws Exception {
+                ArrayList<Integer> outputpoints = new ArrayList<Integer>();
                 while(t.hasNext())
                 {
                     String inputline = t.next();
+                    ArrayList<String[]> inputCoordinates = new ArrayList<String[]>();
                     inputCoordinates.add(inputline.split(","));
                     Double id =Double.parseDouble(inputCoordinates.get(0)[0]);
                     Double x1 =Double.parseDouble(inputCoordinates.get(0)[1]);
                     Double y1 =Double.parseDouble(inputCoordinates.get(0)[2]);
                     if((x1 < broad[2] && x1 > broad[0]) && (y1 > broad[1] && y1 < broad[3]))
                     {
-                        outputpoints.add(id);
+                        outputpoints.add(id.intValue());
                     }
                 }
                 return outputpoints;
@@ -62,5 +53,5 @@ public class SpatialRange
         });
         SpatialRangeOutput.saveAsTextFile(args[2]);
         sc.close();
-    }
+	}
 }
